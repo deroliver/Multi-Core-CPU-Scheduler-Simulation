1f82fdbc1f0247a7344615995c076037f8f0f3e3
deroliver <doliver17@my.whitworth.edu>
2016-03-19 16:43:14 -0700

    Added a Perl script to generate process data, and made a few minor updates to the rest of the project

---
+++ GenData.pl
@@ -0,0 +1,42 @@
+#!/usr/bin/perl
+
+open(DATAFILE, 'processes.txt');
+
+$num_args = $#ARGV + 1;
+if($num_args != 3) {
+	print "\nWrong number of arguments passed\n";
+	print "There should be 3 arguments: Number of processes, MAX CPU burst time, and MAX I/O burst time";
+	exit;
+}
+
+$num_processes = $ARGV[0];
+$max_cpu_burst = $ARGV[1];
+$max_io_burst = $ARGV[2];
+
+$process_id = 0;
+
+if($num_processes > 0 && $max_cpu_burst > 0 && $max_io_burst > 0) {
+	for($i = 0; $i < $num_processes; $i++) {
+		$line = "PID:$process_id ";
+		$process_id += 1;
+
+		$num_bursts = int(rand(5)) + 1;
+
+		$total_bursts = 2 * $num_bursts;
+
+		$line .= "  #OfBursts:$total_bursts ";
+
+		for($j = 0; $j < $num_bursts; $j++) {
+			$cpu_burst = int(rand($max_cpu_burst)) + 1;
+			$io_burst = int(rand($max_io_burst)) + 1;
+			$line .= " $cpu_burst ";
+			$line .= "$io_burst";
+		}
+
+		print "$line\n";
+	}
+} else {
+	print "Invalid arguments passed!\n";
+	exit;
+}
+
