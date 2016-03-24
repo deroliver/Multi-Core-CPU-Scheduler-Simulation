50f4e31650f9269fdc4a156750ab5be71dd4c695
joronia <jaoronia@gmail.com>
2016-03-23 18:08:29 -0700

    Added functions derived from scheduling alg.
    Not implemented yet.

--- CPUSchedulingSimulator/RoundRobin.cs
+++ CPUSchedulingSimulator/RoundRobin.cs
@@ -6,7 +6,44 @@ using System.Threading.Tasks;
 
 namespace CPUSchedulingSimulator
 {
-    class RoundRobin
+    public class RoundRobin : SchedulingAlgorithm
     {
+        // Feel free to change it to something different
+        int timeQuantum = 10;
+
+        public override void moveFromReadyToRunning()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void addNewProcess()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Process getNextProcess()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void moveFromWaitingToReady()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void updateProcessState()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void moveFromRunningToWaiting()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void run(List<Core> CPUS)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
