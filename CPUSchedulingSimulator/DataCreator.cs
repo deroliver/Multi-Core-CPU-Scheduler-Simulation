feaab186f4cc0cd36dd9afe740f834410b9edfdb
joronia <jaoronia@gmail.com>
2016-08-06 03:47:56 -0700

    Checking it out

--- CPUSchedulingSimulator/DataCreator.cs
+++ CPUSchedulingSimulator/DataCreator.cs
@@ -13,18 +13,19 @@ namespace CPUSchedulingSimulator {
             SchedulingAlgorithm FirstComeFirst;
             Simulator S;
 
-            /*
-            for (int i = 0; i < 5; i++) {
-                RoundRobin = new RoundRobin(quantum += 10);
-                for(int j = 0; j < 4; j++) {
-                    S = new Simulator(numCores += 2, RoundRobin);
-                    S.run();
-                }
-                numCores = 0;
-            }
-            */
 
-            for(int i = 0; i < 4; i++) {
+            //for (int i = 0; i < 5; i++) {
+            //    RoundRobin = new RoundRobin(quantum += 10);
+            //    for(int j = 0; j < 4; j++) {
+            //        S = new Simulator(numCores += 2, RoundRobin);
+            //        S.run();
+            //    }
+            //    numCores = 0;
+            //}
+
+
+            for (int i = 0; i < 4; i++)
+            {
                 FirstComeFirst = new MultiLevelFeedBack();
                 S = new Simulator(numCores += 2, FirstComeFirst);
                 S.run();
